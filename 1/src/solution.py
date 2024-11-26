054caa195c8837fc217ca4b4f7a53b72b08e413c
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-26 23:54:42 +0300

    Changed remote origin. 1st task done.

--- 1/src/solution.py
+++ 1/src/solution.py
@@ -4,5 +4,18 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def add_movies(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO movies (title, release_year, duration)
+            VALUES 
+                ('Godfather', 1972, 175),
+                ('The Green Mile', 1999, 189);
+        """)
+        conn.commit()
 
+def get_all_movies(conn):
+    with conn.cursor() as cur:
+        cur.execute('SELECT * FROM movies;')
+        return cur.fetchall()
 # END
